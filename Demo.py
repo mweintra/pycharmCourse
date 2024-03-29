7b74462c767617dee68716e2a7f724a4f3331e0b
qlc-main <mweintra@quadlogic.com>
2024-03-29 13:14:05 -0400

    Third Commit

--- Demo.py
+++ Demo.py
@@ -13,7 +13,7 @@ def print_hello(world):
 
 if __name__ == '__main__':
     hello_world = "%s" % S
-    a = 12
+    a = 10
     print_hello("Hello")
     print("%s" % hello_world)
     print("%s" % "Hello Python")
